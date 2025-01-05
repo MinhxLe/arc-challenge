0be1f723a3d2dfe0072ecd02e6f4e47ee8080040
sidpatelgit <5531708+sidpatelgit@users.noreply.github.com>
2025-01-04 23:05:24 -0800

    fix setup of fine tuned model

--- scripts/dummy.py
+++ scripts/dummy.py
@@ -1,10 +1,40 @@
 from arc.config import all_configs
 from peft import PeftModel
+import peft
 import torch
 from unsloth import FastLanguageModel
 from arc.external.architects import preprocess_model_tokenizer_formatter
 from arc.datasets.seed import Datasets
 from arc.core import Task
+
+
+def fix_dtypes(model, fix_weights=True, fix_quant_states=True):
+    # fix some data types (workaround for unsloth)
+    for module in model.modules():
+        weight = getattr(module, "weight", None)
+        if weight is not None:
+            if torch.is_floating_point(weight):
+                if fix_weights and weight.dtype != model.dtype:
+                    module.to(model.dtype)
+            else:
+                qs = getattr(weight, "quant_state", None)
+                if qs is not None:
+                    if fix_quant_states and qs.dtype != model.dtype:
+                        qs.dtype = model.dtype
+    return model
+
+
+def get_and_fix_peft_weights(store):
+    # change some keys (workaround for added 'modules_to_save')
+    state_dict = peft.load_peft_weights(store)
+    for k in list(state_dict.keys()):
+        if "modules_to_save" in k:
+            del state_dict[k]
+            original_module_key = k.replace(".modules_to_save.", ".original_module.")
+            if original_module_key in state_dict:
+                del state_dict[original_module_key]
+            assert k.replace(".modules_to_save.", ".") in state_dict
+    return state_dict
 
 
 def generate_text(model, tokenizer, message, max_new_tokens=100):
@@ -63,19 +93,27 @@ def test_untuned_model():
 
 
 def test_tuned_model():
-    # Load the adapter
-    trained_model = PeftModel.from_pretrained(
+    checkpoint = "/shared/research/arc_challenge/runs/architects_copy_2024-12-26_keepers/checkpoint-30000/"
+
+    # set up the model, it will get correct LoRA config from checkpoint
+    model = PeftModel.from_pretrained(
         model=model,
-        model_id="/shared/research/arc_challenge/runs/architects_copy_2024-12-26_keepers/checkpoint-30000/",
-        torch_dtype=torch.bfloat16,
+        model_id=checkpoint,
         device_map="cuda",
     )
 
-    # merge in the LoRA weights
-    trained_model.merge_and_unload()
-    FastLanguageModel.for_inference(trained_model)
-    post_train = generate_text(
-        trained_model, tokenizer, train_example, max_new_tokens=10000
+    weight_set_result = peft.set_peft_model_state_dict(
+        model,
+        get_and_fix_peft_weights(checkpoint),
     )
+    assert (
+        not weight_set_result.unexpected_keys
+    ), "error loading weights - some keys not available in model"
+
+    model = fix_dtypes(model.merge_and_unload())
+
+    FastLanguageModel.for_inference(model)
+    post_train = generate_text(model, tokenizer, train_example, max_new_tokens=10000)
     return post_train[:100]
-    # '7!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    # architects copy:  '707000707\n707000707\n770000770\n707000707\n707000707\n770000770\n707707000\n707707000\n770770000\n'
+    # my rework:        '707000707\n707000707\n770000770\n707000707\n707000707\n770000770\n707707000\n707707000\n770770000\n'
